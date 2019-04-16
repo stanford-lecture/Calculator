13aad83dd746b1e27cd11f89e5b0acd66cab7b64
deokho.lee <emeralho@gmail.com>
2019-04-16 23:36:30 +0900

    Add Save and Restore

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -12,10 +12,12 @@ class CalculatorBrain
 {
     
     private var accumulator = 0.0
+    private var internalProgram = [AnyObject]()
     
     
     func setOperand(operand: Double) {
         accumulator = operand
+        internalProgram.append(operand as AnyObject)
     }
     
     private var operations: Dictionary<String, Operation> = [
@@ -40,7 +42,7 @@ class CalculatorBrain
     }
     
     func performOperation(symbol: String) {
-        
+        internalProgram.append(symbol as AnyObject)
         if let operation = operations[symbol] {
             switch operation {
             case .Constant(let value):
@@ -73,6 +75,37 @@ class CalculatorBrain
         var binaryFunction: (Double, Double) -> Double
         var firstOperand: Double
     }
+    
+    typealias PropertyList = AnyObject
+    var program: PropertyList {
+        get {
+            return internalProgram as CalculatorBrain.PropertyList
+            // 내부의 데이터구조를 외부의 호출자에게 리턴해주는 것이 이상할 수 있지만
+            // 배열은 value 타입이기 때문에 복사가 된다.
+            // 결국 복사된 값을 돌려주는 것이기때문에 문제가 발생하지 않는다.
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    } else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    func clear() {
+        accumulator = 0
+        pending = nil
+        internalProgram.removeAll()
+        
+    }
+    
     
     var result: Double {
         get {
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
     
     private var userIsInTheMiddleofTyping = false
     
+    private var brain = CalculatorBrain()
+    
+    private var displayValue: Double {
+        get {
+            // display에 항상 숫자만 입력받을 거라고 가정하는 코드 (만약 hello같은 string이 들어간다면 앱은 크래시 발생)
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
     @IBAction private func touchDigit(_ sender: UIButton) {
         
         let digit = sender.currentTitle!
@@ -27,18 +39,6 @@ class ViewController: UIViewController {
         
         userIsInTheMiddleofTyping = true
     }
-    
-    private var displayValue: Double {
-        get {
-            // display에 항상 숫자만 입력받을 거라고 가정하는 코드 (만약 hello같은 string이 들어간다면 앱은 크래시 발생)
-            return Double(display.text!)!
-        }
-        set {
-            display.text = String(newValue)
-        }
-    }
-    
-    private var brain = CalculatorBrain()
     
     @IBAction private func performOperation(_ sender: UIButton) {
         
@@ -55,6 +55,19 @@ class ViewController: UIViewController {
     
     func testMethod(testParam: String) {
         print(testParam)
+    }
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
     }
 }
 
